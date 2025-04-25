5e305618138d9953be854f82ccad408bf228c02a
Antec-Louise <lam16louise@hotmail.fr>
2025-04-25 17:31:19 +0200

    🚧 scripts: Refactor PDB correction logic and remove unused code.

--- scripts/correct_pdb.py
+++ scripts/correct_pdb.py
@@ -1,41 +1,21 @@
-from Bio.PDB import PDBParser, PDBIO, StructureBuilder, Select
-from Bio.PDB.Structure import Structure
-from Bio.PDB.Model import Model
-from Bio.PDB.Chain import Chain
-from Bio.PDB.Residue import Residue
+def read(pdb_file: str, pred_start, exp_start):
+    res_num = exp_start
+    old_res_num = str(pred_start)
+    with open("scripts/corrected.pdb", "w") as f_corr:
+        with open("scripts/to_correct.pdb", "r") as f_to_corr:
+            for line in f_to_corr:
+                if line[77:78] == "H":
+                    continue
+                if line[22:26] != old_res_num:
+                    res_num += 1
+                    old_res_num = line[22:26]
+                corrected_line = (
+                    line[:22] + str(res_num) + line[26:72] + "A" + line[73:]
+                )
+                f_corr.write(corrected_line)
 
-class NoHydrogens(Select):
-    def accept_atom(self, atom):
-        return atom.element != "H"
 
-def correct_pdb(pdb_file: str, inf: int, sup: int, new_inf: int):
-    parser = PDBParser(QUIET=True)
-    original = parser.get_structure("prot", pdb_file)
-
-    new_structure = Structure("new")
-    new_model = Model(0)
-    new_chain = Chain("A")
-
-    p = new_inf
-    for model in original:
-        for chain in model:
-            for residue in chain:
-                res_id = residue.id[1]
-                if inf <= res_id <= sup:
-                    new_residue = Residue((' ', p, ' '), residue.resname, residue.segid)
-                    for atom in residue:
-                        if atom.element != "H":
-                            new_residue.add(atom.copy())
-                    new_chain.add(new_residue)
-                    p += 1
-
-    new_model.add(new_chain)
-    new_structure.add(new_model)
-
-    io = PDBIO()
-    io.set_structure(new_structure)
-    io.save("structure_modifiée.pdb")
-
-if __name__ == '__main__':
-    PDB_FILE = "aligned-pred.pdb"
-    correct_pdb(PDB_FILE, 261, 316, 283)
+if __name__ == "__main__":
+    pred_start = 1213
+    exp_start = 1235
+    read("scripts/to_correct.pdb", pred_start, exp_start)
